f8dee3226b3a950eaca383bd2a2dfa90259b4a9d
Ismael Yahyaoui Racine <iyahoui-@student.42quebec.com>
2021-10-22 17:24:51 -0400

    updated gnl_bonus to protect against fd<0 && fd>OPEN_MAX

--- get_next_line/get_next_line_utils_bonus.c
+++ get_next_line/get_next_line_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: iyahoui- <iyahoui-@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 16:07:04 by iyahoui-          #+#    #+#             */
-/*   Updated: 2021/09/30 08:19:59 by iyahoui-         ###   ########.fr       */
+/*   Updated: 2021/10/21 11:51:53 by iyahoui-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,34 +31,30 @@ size_t	strlen_c(char *s, char c)
 }
 
 //Slightly modified version from the libft where the dest is freed
-char	*ft_strjoin_free(char *s1, char const *s2, size_t n)
+char	*ft_strjoin_free(char *s1, char const *s2)
 {
 	size_t	tot_len;
 	char	*s_joined;
-	size_t	i;
+	int		i;
 
 	i = 0;
 	tot_len = strlen_c(s1, 0) + strlen_c((char *)s2, 0) + 1;
 	s_joined = malloc(tot_len);
 	if (!s_joined)
 		return (NULL);
-	if (s1 || s1[i])
+	if (s1)
 	{
-		while (s1[i] && i < n)
+		while (s1[i])
 			*s_joined++ = s1[i++];
-		if (s1[0])
-			free (s1);
+		free (s1);
 	}
-	i = 0;
 	if (s2)
-	{
-		while (s2[i] && i < n)
-			*s_joined++ = s2[i++];
-	}
+		while (*s2)
+			*s_joined++ = *s2++;
 	*s_joined++ = 0;
 	return (s_joined - tot_len);
 }
-/*
+
 char	*ft_strncpy(char *dest, char *src, size_t n)
 {
 	size_t	i;
@@ -74,4 +70,15 @@ char	*ft_strncpy(char *dest, char *src, size_t n)
 	dest[i] = 0;
 	return (dest);
 }
-*/
+
+//That's some of the dumbest shit y'all ever witness. 
+//Just to save a couple lines of code.
+char	*ft_free(void *ptr)
+{
+	if (ptr)
+	{
+		free (ptr);
+		ptr = NULL;
+	}
+	return (NULL);
+}
